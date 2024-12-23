3292ee101a47fe0677a042f67cd1a5ab18f52d0c
petezahv4 <petezahiscool@gmail.com>
2024-12-23 10:45:37 -0500

    Create uv.config.js

---
+++ storage/static/uv/uv.config.js
@@ -0,0 +1,12 @@
+
+
+self.__uv$config = {
+    prefix: '/static/ixl/',
+    bare:'https://v2202412246404304352.megasrv.de/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
