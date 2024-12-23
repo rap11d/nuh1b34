f138aa4378b278286e5d744bf74efa05e8302b08
petezahv4 <petezahiscool@gmail.com>
2024-12-23 11:52:37 -0500

    Update settings.js

--- storage/js/settings.js
+++ storage/js/settings.js
@@ -330,7 +330,7 @@ function handleCheckboxChange() {
 				localStorage.removeItem('launchType');
 				localStorage.removeItem('blob');
 				localStorage.removeItem('aboutBlank');
-				window.open('/settings.html/#/blank');
+				window.open('/~/#/blank');
 				window.close();
 				const urlMap = {
 					'None (Default)': 'https://google.com',
