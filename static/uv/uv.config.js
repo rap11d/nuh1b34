cd2004eea48549ac4e9ebf263324a8941ddda818
petezahv4 <petezahiscool@gmail.com>
2024-12-23 11:01:16 -0500

    Create uv.config.js

---
+++ static/uv/uv.config.js
@@ -0,0 +1,12 @@
+
+
+self.__uv$config = {
+    prefix: '/static/ixl/',
+    bare:'https://static.99.135.156.178.clients.your-server.de/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
