f4b713d7f07c3eec139d5bfbfc99b90959e7745f
petezahv4 <petezahiscool@gmail.com>
2025-01-08 11:18:33 -0500

    Delete storage/js directory

--- storage/js/themes.js
+++
@@ -1,7 +0,0 @@
-
-let selectedTheme =
-	localStorage.getItem('dropdown-selected-text-themes') || 'Default';
-selectedTheme = selectedTheme.replace(/ /g, '');
-selectedTheme = selectedTheme.replace(/-/g, '');
-selectedTheme = selectedTheme.toLowerCase();
-document.body.classList.add(selectedTheme);
