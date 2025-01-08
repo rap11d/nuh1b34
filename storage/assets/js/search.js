901dc1f7af61f7578891cd2ff6bd13ae7e5ec7cd
petezahv4 <petezahiscool@gmail.com>
2025-01-08 11:19:56 -0500

    Add files via upload

---
+++ storage/assets/js/search.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    function filterItems() {
+        const searchInput = document.getElementById('search-games').value.toLowerCase();
+        document.querySelectorAll('.image-item').forEach(item => {
+            const label = item.dataset.label.toLowerCase();
+            item.style.display = label.includes(searchInput) ? '' : 'none';
+        });
+    }
+
+    document.getElementById('search-games').addEventListener('input', filterItems);
+});
