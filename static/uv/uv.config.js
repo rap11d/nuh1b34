5a05a4abf3cb734e6468c407e5e710f144792b4d
pineapple-petezah <petezahindustries@gmail.com>
2025-01-17 11:03:08 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -7,7 +7,7 @@
 
 self.__uv$config = {
     prefix: '/static/ixl/',
-    bare:'https://v2202412246404304352.megasrv.de/bare/',
+    bare:'https://uhbm.arybarbosa.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
